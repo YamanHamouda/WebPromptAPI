320628ce0950075fcd5e4d33ddb4a0096ff19fe1
YamanHamouda <mhdyamanhamouda21@gmail.com>
2026-01-19 15:17:50 -0800

    add fetch_url_soup(url) function. input url. output soup

--- src/scrape.py
+++ src/scrape.py
@@ -1,1 +1,14 @@
+import requests
 from bs4 import BeautifulSoup
+
+# url = "https://www.britannica.com/topic/Islam" #example
+def fetch_url_soup(url):
+    
+
+    response = requests.get(url)
+    html_string = response.text
+    # print(html_string)
+
+    soup = BeautifulSoup(html_string, "html.parser")
+    return soup
+
