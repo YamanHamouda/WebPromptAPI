641231f5d992bd7fd101c6abf169bf5f025785d7
YamanHamouda <mhdyamanhamouda21@gmail.com>
2026-01-19 15:40:22 -0800

    added a function to focus on important tag indicators

--- src/scrape.py
+++ src/scrape.py
@@ -11,8 +11,16 @@ def fetch_url_soup(url) -> BeautifulSoup: #1
     soup = BeautifulSoup(html_string, "html.parser")
     return soup
 
+def get_main_container(soup) -> BeautifulSoup: #2
+    return (
+        soup.find("main")
+        or soup.find("article")
+        or soup.find("div", id="content")
+        or soup.find("div", class_="content")
+        or soup.body
+    )
 
-def strip_uneeded_tags(soup) -> None: #
+def strip_uneeded_tags(soup) -> None: #3
     uneeded_tags = [
         "script", "style", "noscript",
         "header", "footer", "nav", "aside",
@@ -24,6 +32,7 @@ def strip_uneeded_tags(soup) -> None: #
 
 def parse_url(url):
     soup = fetch_url_soup(url) 
+    soup = get_main_container(soup)
     strip_uneeded_tags(soup) #no return
 
 parse_url(url)
